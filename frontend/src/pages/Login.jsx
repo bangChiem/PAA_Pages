6355c89bc6ada37ecd9ec88fda0d62c1be45f07a
Sam <samomat2005@gmail.com>
2026-01-31 09:52:51 -0500

    Updated login path

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("http://localhost:3000/auth/login", {
         email,
         password,
       });
